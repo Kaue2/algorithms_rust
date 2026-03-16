d13649154dafd14d65bbd0899e1440eba2d8bcfc
Kauê Dos Anjos <kaueo.anjos@gmail.com>
2026-03-16 15:54:11 -0300

    add: implementar métodos para inserção head e tail

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,4 +1,6 @@
-use std::{marker::PhantomData, ptr::NonNull};
+use std::marker::PhantomData;
+use std::ptr;
+use std::ptr::NonNull;
 
 pub struct Node<T> {
     pub val: T,
@@ -23,6 +25,12 @@ pub struct LinkedList<T> {
     marker: PhantomData<Box<Node<T>>>,
 }
 
+impl<T> Default for LinkedList<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl<T> LinkedList<T> {
     pub fn new() -> Self {
         Self {
@@ -32,10 +40,32 @@ impl<T> LinkedList<T> {
             marker: PhantomData,
         }
     }
-}
 
-impl<T> Default for LinkedList<T> {
-    fn default() -> Self {
-        Self::new()
+    // insere um item no início da lista
+    pub fn insert_at_head(&mut self, obj: T) {
+        let mut node = Box::new(Node::new(obj)); // box é um ponteiro para uma alocação no heap
+        node.next = self.head;
+        node.prev = None;
+        let node_ptr = NonNull::new(Box::into_raw(node));
+        match self.head {
+            None => self.tail = node_ptr,
+            Some(head_ptr) => unsafe { (*head_ptr.as_ptr()).prev = node_ptr },
+        }
+        self.head = node_ptr;
+        self.length += 1;
+    }
+
+    // insere um item no final da lista
+    pub fn insert_at_tail(&mut self, obj: T) {
+        let mut node = Box::new(Node::new(obj));
+        node.prev = self.tail;
+        node.next = None;
+        let node_ptr = NonNull::new(Box::into_raw(node));
+        match self.tail {
+            None => self.head = node_ptr,
+            Some(tail_ptr) => unsafe { (*tail_ptr.as_ptr()).next = node_ptr },
+        }
+        self.tail = node_ptr;
+        self.length += 1;
     }
 }
